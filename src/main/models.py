5265735d810944c57abbe8a660da0651e8b9a05d
whdev36 <wisdomhunter36@gmail.com>
2024-11-29 23:54:13 -0800

    news and category model

--- src/main/models.py
+++ src/main/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
+from django.utils.text import slugify
 
-# Create your models here.
+# Category
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name.lower())
+        super().save(self, *args, **kwargs)
+
+    def __str__(self):
+        return self.name
+
+# News
+class News(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to=f'news/{datetime.year}/', blank=True, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
